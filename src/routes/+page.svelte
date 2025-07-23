447f7859a318f96091bc2df10002b62065962468
sergejbog <serbogatinoski@gmail.com>
2025-07-23 20:58:29 +0200

    Implement carousel component and update main page layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Carousel from './Carousel.svelte';
+
+  let items = [
+		{ id: 1, text: 'Logo A' },
+		{ id: 2, text: 'Logo B' },
+		{ id: 3, text: 'Logo C' },
+		{ id: 4, text: 'Logo D' },
+		{ id: 5, text: 'Logo E' }
+	];
+</script>
+
+<main class="mx-auto w-full max-w-lg py-8">
+	<h1 class="mt-20 text-center text-4xl text-gray-400">Carousel Coding Challenge</h1>
+
+	<div class="relative w-full overflow-hidden mt-20">
+		<Carousel {items}/>
+	</div>
+</main>
---
+++ src/routes/Carousel.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	interface Props {
+		items: { id: number; text: string }[];
+	}
+
+	const { items }: Props = $props();
+</script>
+
+<div class="flex w-full">
+	{#each items as item (item.id)}
+		<div class="w-1/3 flex-shrink-0 p-4">
+			<div class="flex h-24 items-center justify-center rounded-lg bg-gray-700">
+				<span class="font-bold text-white">{item.text}</span>
+			</div>
+		</div>
+	{/each}
+</div>
